115775f3c7382c5ebc006954533556ec8c5d4509
Ana Vedoy <56606639+anavedoy@users.noreply.github.com>
2021-04-08 22:35:35 -0300

    trabalho mt versao 5

--- trabalho_tec.py
+++ trabalho_tec.py
@@ -3,7 +3,7 @@
 mt_input = open("input_file.txt", 'r')
 linha = mt_input.readline()
 
-class aresta:
+class aresta: 
     def __init__(self, estado_fim, direcao, leitura, escrita):
         self.estado_fim = estado_fim
         self.direcao = direcao
@@ -59,7 +59,6 @@ class grafo:
                 char_proximo_estado = linha[8]
 
                 aresta_da_interacao = aresta(char_proximo_estado,char_direcao,char_simbolo_leitura, char_simbolo_escrita)
-                #tenho q achar o estado e tacar a aresta no doido
                 self.check_se_estado_existe_e_inclui_aresta(char_estado_atual,aresta_da_interacao, self.estados)
 
     def printar_grafo(self):
@@ -74,24 +73,66 @@ class grafo:
                 print("------------")
             print("---------------------------------------")
     
-    def transf_semi_p_inf(self):        
+    def transf_semi_p_inf(self):  
+        # no inicio coloco que a fita vai ter um marcador com o simbolo £
+        # entao vai existir dois estados auxiliares para por £ na esquerda da palavra de input
+        # i * * l j
+        # j _ £ r 0
+        estado_aux_i = estado("i") 
+        aresta_aux_i = aresta("j", "l", "*", "*")
+        estado_aux_i.arestas.append(aresta_aux_i)
+
+        estado_aux_j = estado("j")
+        aresta_aux_j = aresta("0", "r", "_", "£")
+        estado_aux_j.arestas.append(aresta_aux_j)
+     
+        estados_semi_p_infi = list()
+        
+        estados_semi_p_infi.append(estado_aux_i)
+        estados_semi_p_infi.append(estado_aux_j)
+
         for e in self.estados:
+            estados_semi_p_infi.append(e)
+
+        estados_auxiliares = list()
+
+        # fazer uma copia de cada estado, 
+        # seja um estado H e sua copia HH: 
+        #   onde se H ler £ vai para copia, onde a instruçao le £ e escreve £, e cabecote vai pra direita
+        #   entao se vai para o estado HH com o cabecote ja fora de £ 
+
+        alfabeto = ["a","b","c","d","e","f","g","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+        aux=0
+        for e in estados_semi_p_infi:
+            if(e.nome != "i") and (e.nome != "j"):
+                string = alfabeto[aux]
+                estado_aux=estado(string)
+                for a in e.arestas:
+                    estado_aux.arestas.append(a)
+                e.arestas.append(aresta(string, "r", "£", "£" ))
+                estados_auxiliares.append(estado_aux)
+                aux=aux+1
+        
+        for e in estados_auxiliares:
+            estados_semi_p_infi.append(e)
+
+        return estados_semi_p_infi
+
+    #<current state> <current symbol> <new symbol> <direction> <new state>
+    def exportar_grafo(self, lista_estados):
+        mt_output = open("output_file.txt", "w")
+        for e in lista_estados:
             for a in e.arestas:
-                if(a.direcao=="l"):
-                    for e_e in self.estados:
-                        if(e_e.nome==a.estado_fim):
-                            e_e.arestas.append(aresta(e.nome,"d", "f", "f"))
+                frase = e.nome + " " + a.leitura + " " + a.escrita + " " + a.direcao + " "
+                mt_output.write(frase+ a.estado_fim + "\n")
+        
+        mt_output.close()
 
 
 if linha==";S\n":
     g=grafo()
     g.incluir_conteudo()
     g.fazer_grafo()
-    g.transf_semi_p_inf()
-    g.printar_grafo()
-    #qualquer estado que tiver algo indo pra esquerda, adiciona uma ares
-    #estado: aresta (estado_fim, direcao, leitura, escrita)
-    #se direcao for esquerda:
-    #   estado_fim precisa adicionar uma aresta(estado_inicial, direita,branco, branco)
-
-
+    lista_estados_auxiliar = g.transf_semi_p_inf()
+    g.exportar_grafo(lista_estados_auxiliar)
+   
